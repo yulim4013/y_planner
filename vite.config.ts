85322271ccbfd2d6356be4046c130b7cff94875c
yulim <yulim4013@gmail.com>
2026-03-29 21:24:44 +0900

    fix: base path를 y_planner로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/life-planner/',
+  base: '/y_planner/',
   server: {
     port: 5173,
     host: true,
